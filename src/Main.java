5cabed4bf1c5dd4317dc53fc99db6a9153ed5437
imkazaimka <Imronbekse@gmail.com>
2025-04-03 21:32:41 +0100

    Main class and integrating of menu added.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ShapeManager manager = new ShapeManager();
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+            printMainMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    manager.addShape();
+                    break;
+                case 2:
+                    manager.listShapes();
+                    break;
+                case 3:
+                    manager.translateShapes();
+                    break;
+                case 4:
+                    manager.scaleShapes();
+                    break;
+                case 5:
+                    manager.viewShapeDetails();
+                    break;
+                case 0:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option, please try again.");
+            }
+        }
+
+        System.out.println("Exiting Shape Management Application. Goodbye!");
+    }
+
+    static void printMainMenu() {
+        System.out.println("\n***** Shape Management Menu *****");
+        System.out.println("1. Add a Shape");
+        System.out.println("2. List all Shapes");
+        System.out.println("3. Translate all Shapes");
+        System.out.println("4. Scale all Shapes");
+        System.out.println("5. View Shape Details");
+        System.out.println("0. Exit");
+        System.out.print("Enter your choice: ");
     }
 }
